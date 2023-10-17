1ccc4044efb5c2e052de414cf6242891337187ed
Lovmong <1967471283@qq.com>
2023-10-17 14:21:49 +0800

    add  usb_current, fix bug of power_source

--- example/full_print.py
+++ example/full_print.py
@@ -11,10 +11,11 @@ while True:
     print(f"Battery current:    {data_buffer['battery_current']} mA")
     print(f"Battery percentage: {data_buffer['battery_percentage']} %")
     print(f"USB voltage:        {data_buffer['usb_voltage']} mV")
+    print(f"USB current:        {data_buffer['usb_current']} mA")
     print(f"Output voltage:     {data_buffer['output_voltage']} mV")
     print(f"Output current:     {data_buffer['output_current']} mA")
     print(f"Charging:           {'Charging' if data_buffer['is_charging'] else 'Not charging'}")
-    print(f"Power source:       {'USB' if data_buffer['power_source'] else 'Battery'}")
+    print(f"Power source:       {'Battery' if data_buffer['power_source'] == spc.BATTERY else 'USB'}")
     print(f"USB plugged in:     {'Plugged in' if data_buffer['is_usb_plugged_in'] else 'Unplugged'}")
     print(f"Fan speed:          {data_buffer['fan_speed']} %")
 

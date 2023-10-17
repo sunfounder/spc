f42e1fa0b4ef580da950734eac848f2f7c804cbb
Lovmong <1967471283@qq.com>
2023-10-17 14:22:08 +0800

    optimize print

--- spc/__init__.py
+++ spc/__init__.py
@@ -10,16 +10,17 @@ def monitor(spc):
 
 def print_all_data_once(spc):
     data_buffer = spc.read_all()
-    print(f"Battery voltage: {data_buffer['battery_voltage']} mV")
-    print(f"Battery current: {data_buffer['battery_current']} mA")
+    print(f"Battery voltage:    {data_buffer['battery_voltage']} mV")
+    print(f"Battery current:    {data_buffer['battery_current']} mA")
     print(f"Battery percentage: {data_buffer['battery_percentage']} %")
-    print(f"USB voltage: {data_buffer['usb_voltage']} mV")
-    print(f"Output voltage: {data_buffer['output_voltage']} mV")
-    print(f"Output current: {data_buffer['output_current']} mA")
-    print(f"Charging: {'Charging' if data_buffer['is_charging'] else 'Not charging'}")
-    print(f"Power source: {'USB' if data_buffer['power_source'] else 'Battery'}")
-    print(f"USB plugged in: {'Plugged in' if data_buffer['is_usb_plugged_in'] else 'Unplugged'}")
-    print(f"Fan speed: {data_buffer['fan_speed']} %")
+    print(f"USB voltage:        {data_buffer['usb_voltage']} mV")
+    print(f"USB current:        {data_buffer['usb_current']} mA")
+    print(f"Output voltage:     {data_buffer['output_voltage']} mV")
+    print(f"Output current:     {data_buffer['output_current']} mA")
+    print(f"Charging:           {'Charging' if data_buffer['is_charging'] else 'Not charging'}")
+    print(f"Power source:       {'Battery' if data_buffer['power_source'] == spc.BATTERY else 'USB'}")
+    print(f"USB plugged in:     {'Plugged in' if data_buffer['is_usb_plugged_in'] else 'Unplugged'}")
+    print(f"Fan speed:          {data_buffer['fan_speed']} %")
 
 def print_all_data_once_json(spc):
     import json

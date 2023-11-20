3f5fadad1372afa3c4862b4774521513c2a2e93e
Lovmong <1967471283@qq.com>
2023-11-20 11:25:05 +0800

    add battery_capactiy

--- example/full_print.py
+++ example/full_print.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from spc import SPC
+from spc.spc import SPC
 import time
 
 spc = SPC()
@@ -7,16 +7,18 @@ spc = SPC()
 while True:
     print('----------------------------------------------')
     data_buffer = spc.read_all()
-    print(f"Battery voltage:    {data_buffer['battery_voltage']} mV")
-    print(f"Battery current:    {data_buffer['battery_current']} mA")
-    print(f"Battery percentage: {data_buffer['battery_percentage']} %")
-    print(f"USB voltage:        {data_buffer['usb_voltage']} mV")
-    print(f"USB current:        {data_buffer['usb_current']} mA")
-    print(f"Output voltage:     {data_buffer['output_voltage']} mV")
-    print(f"Output current:     {data_buffer['output_current']} mA")
-    print(f"Charging:           {'Charging' if data_buffer['is_charging'] else 'Not charging'}")
-    print(f"Power source:       {'Battery' if data_buffer['power_source'] == spc.BATTERY else 'USB'}")
-    print(f"USB plugged in:     {'Plugged in' if data_buffer['is_usb_plugged_in'] else 'Unplugged'}")
-    print(f"Fan speed:          {data_buffer['fan_speed']} %")
+    print(f"        Board name:       {data_buffer['board_id']}")
+    print(f"   Battery voltage:       {data_buffer['battery_voltage']} mV")
+    print(f"   Battery current:       {data_buffer['battery_current']} mA")
+    print(f"  Battery capactiy:       {data_buffer['battery_capactiy']} mAh")
+    print(f"Battery percentage:       {data_buffer['battery_percentage']} %")
+    print(f"       USB voltage:       {data_buffer['usb_voltage']} mV")
+    print(f"       USB current:       {data_buffer['usb_current']} mA")
+    print(f"    Output voltage:       {data_buffer['output_voltage']} mV")
+    print(f"    Output current:       {data_buffer['output_current']} mA")
+    print(f"          Charging:       {'Charging' if data_buffer['is_charging'] else 'Not charging'}")
+    print(f"      Power source:       {'Battery' if data_buffer['power_source'] == spc.BATTERY else 'USB'}")
+    print(f"    USB plugged in:       {'Plugged in' if data_buffer['is_usb_plugged_in'] else 'Unplugged'}")
+    print(f"         Fan speed:       {data_buffer['fan_speed']} %")
 
     time.sleep(1)

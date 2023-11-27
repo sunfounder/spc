33358aebb6f64be705c07b2617f24f38262d0c22
Lovmong <1967471283@qq.com>
2023-11-27 16:33:25 +0800

    fix bug of get cpu usage

--- spc/system_status.py
+++ spc/system_status.py
@@ -3,6 +3,7 @@ import subprocess
 import shutil
 from .utils import Logger
 from .ha_api import HA_API
+import time
 
 log = Logger('SYSTEM_STATUS')
 ha = HA_API()
@@ -25,16 +26,64 @@ def get_ram_info():
     ram = subprocess.check_output(cmd, shell=True).decode()
     return(ram.split())
 
-# Return % of CPU used as a character string
+# # Return % of CPU used as a character string
+'''
+Deprecated, high CPU usage when running top -bn1 in bookworm.
+'''
+# def get_cpu_usage():
+#     cmd = "top -bn1 |awk 'NR==3 {print $8}'"
+#     try:
+#         CPU_usage = subprocess.check_output(cmd,shell=True).decode().replace(',', '.')
+#         CPU_usage = round(100 - float(CPU_usage),1)
+#     # except Exception as e:
+#     #     log(f'get_cpu_usage error: {e}', level='ERROR')
+#     #     return 0.0
+#     except:
+#         CPU_usage = 0.0
+#     return CPU_usage
+
+# Return CPU usage percentage as a float
 def get_cpu_usage():
-    cmd = "top -bn1 |awk 'NR==3 {print $8}'"
+    '''
+    cmd = 'cat /proc/stat |grep -w cpu'
+    # user	nice	system	idle	iowait	irq	softirq 0 0 0
+    cpu_total= user + nice + system + idle + iowait + irq + softirq
+    cpu_total = cpu_total_2 - cpu_total_1
+    user = user_2 - user_1
+    nice = nice_2 - nice_1
+    idle = idle_2 - idle_1
+    ...
+
+    # cpu usage = (1 - idle / cpu_total) * 100%
+    # cpu usage = (user + nice + system)/ cpu_total * 100%
+    '''
+    cmd = 'cat /proc/stat |grep -w cpu'
+    cpu_total = 0
+    user = 0
+    nice = 0
+    system = 0
+
     try:
-        CPU_usage = subprocess.check_output(cmd,shell=True).decode().replace(',', '.')
-        CPU_usage = round(100 - float(CPU_usage),1)
+        for _ in range(2):
+            cpu_info = []
+            cpu_info = list(subprocess.check_output(cmd,shell=True).decode().split())
+            cpu_info = [int(cpu_info[i]) for i in range(1, 8)]
+            # print(cpu_info)
+            cpu_total = sum(cpu_info) - cpu_total
+            user = cpu_info[0] - user
+            nice = cpu_info[1] - nice
+            system = cpu_info[2] - system
+            # print(cpu_total, user, nice, system)
+            time.sleep(.1)
+
+        cpu_usage = (user + nice + system)/ cpu_total * 100
+        # print(cpu_usage)
+
     except Exception as e:
-        log(f'get_cpu_usage error: {e}', level='ERROR')
-        return 0.0
-    return CPU_usage
+        print(f'get_cpu_usage error: {e}', level='ERROR')
+        cpu_usage = 0.0
+
+    return cpu_usage
 
 # Return information about disk space as a list (unit included)
 # Index 0: total disk space

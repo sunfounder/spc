7bc2c60f7b09ab749fa9fb74c8fc4a7e2a4dcfc7
Lovmong <1967471283@qq.com>
2023-12-11 13:48:31 +0800

    optimize

--- example/full_print.py
+++ example/full_print.py
@@ -5,22 +5,28 @@ import time
 spc = SPC()
 
 while True:
-    # print('----------------------------------------------')
+    # Read the data before clearing the screen，to retain the last data when an error occurs.
+    data_buffer = spc.read_all()
+
     print("\033[H\033[J", end='')  # clear terminal windows
 
-    data_buffer = spc.read_all()
-    print(f"        Board name:       {data_buffer['board_id']}")
-    print(f"   Battery voltage:       {data_buffer['battery_voltage']} mV")
-    print(f"   Battery current:       {data_buffer['battery_current']} mA")
-    print(f"  Battery capactiy:       {data_buffer['battery_capactiy']} mAh")
-    print(f"Battery percentage:       {data_buffer['battery_percentage']} %")
-    print(f"       USB voltage:       {data_buffer['usb_voltage']} mV")
-    print(f"       USB current:       {data_buffer['usb_current']} mA")
-    print(f"    Output voltage:       {data_buffer['output_voltage']} mV")
-    print(f"    Output current:       {data_buffer['output_current']} mA")
-    print(f"          Charging:       {'Charging' if data_buffer['is_charging'] else 'Not charging'}")
-    print(f"      Power source:       {'Battery' if data_buffer['power_source'] == spc.BATTERY else 'USB'}")
-    print(f"    USB plugged in:       {'Plugged in' if data_buffer['is_usb_plugged_in'] else 'Unplugged'}")
-    print(f"         Fan speed:       {data_buffer['fan_speed']} %")
+    print(f"Board name:               {data_buffer['board_id']}")
+    if ('battery'  in spc.device.peripherals):
+        print(f"Battery voltage:          {data_buffer['battery_voltage']} mV")
+        print(f"Battery current:          {data_buffer['battery_current']} mA")
+        print(f"Battery capactiy:         {data_buffer['battery_capactiy']} mAh")
+        print(f"Battery percentage:       {data_buffer['battery_percentage']} %")
+    if ('usb_in'  in spc.device.peripherals):
+        print(f"USB voltage:              {data_buffer['usb_voltage']} mV")
+        print(f"USB current:              {data_buffer['usb_current']} mA")
+    if ('output'  in spc.device.peripherals):
+        print(f"Output voltage:           {data_buffer['output_voltage']} mV")
+        print(f"Output current:           {data_buffer['output_current']} mA")
+    if ('battery'  in spc.device.peripherals):
+        print(f"Charging:                 {'Charging' if data_buffer['is_charging'] else 'Not charging'}")
+        print(f"Power source:             {'Battery' if data_buffer['power_source'] == spc.BATTERY else 'USB'}")
+        print(f"USB plugged in:           {'Plugged in' if data_buffer['is_usb_plugged_in'] else 'Unplugged'}")
+    if ('fan'  in spc.device.peripherals):
+        print(f"Fan speed:                {data_buffer['fan_speed']} %")
 
     time.sleep(1)

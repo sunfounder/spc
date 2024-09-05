a7a5aad265812e85b4ba3fd8e6ff46bc0675c6c5
Lovmong <1967471283@qq.com>
2024-09-05 10:58:39 +0800

    add pipower5

--- spc/devices.py
+++ spc/devices.py
@@ -40,7 +40,27 @@ class Devices():
                 'default_on',
                 'shutdown_percentage',
             ],
-        }
+        },
+        0x5C:{
+            'name': 'PiPower 5',
+            'id': 'pipower_5',
+            "address": 0x5C,
+            "mode": 'normal',
+            "peripherals": [
+                'input_voltage',
+                'output_voltage',
+                'output_current',
+                'battery_voltage',
+                'battery_current',
+                'battery_percentage',
+                'power_source',
+                'is_input_plugged_in',
+                'is_charging',
+                'shutdown_request',
+                'shutdown_percentage',
+                'default_on',
+            ],
+        },
     }
     ADDRESS = DEVICES.keys()
     def __init__(self, address):
